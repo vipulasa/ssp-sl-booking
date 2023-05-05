4b3bc3d3d3fe834ea43fe5ca7443c9c2357a684b
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2023-05-05 11:27:33 +0530

    home page hotel view added with HTML grid to show 4 items in a row

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,108 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Home Page
+            <div class="container mx-auto">
+
+                <div class="grid grid-cols-4 gap-4">
+
+                    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology
+                                    acquisitions 2021</h5>
+                            </a>
+                            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise
+                                technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                View
+                                <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology
+                                    acquisitions 2021</h5>
+                            </a>
+                            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise
+                                technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                View
+                                <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology
+                                    acquisitions 2021</h5>
+                            </a>
+                            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise
+                                technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                View
+                                <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">Noteworthy technology
+                                    acquisitions 2021</h5>
+                            </a>
+                            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise
+                                technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                View
+                                <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
 });
+
+Route::get('hotel/{id}', function($id){
+    return view('hotel.show', [
+        'hotel' => $id
+    ]);
+})
+//->where('id', '[0-9]+')
+->name('hotel.show');
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
